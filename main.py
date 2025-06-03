c06ddd50c06bd94009d659621d11fb5b96af61cb
kimorrow321 <awalidhilman@gmail.com>
2025-06-03 13:28:26 +0700

    Add script.js on main.py

--- main.py
+++ main.py
@@ -77,41 +77,7 @@ base_style_and_script = '''
         }
     </style>
 
-    <script>
-        window.addEventListener('DOMContentLoaded', () => {{
-            const audio = document.getElementById("bgAudio");
-            const flip = document.getElementById("flipSound");
-            const lastTime = sessionStorage.getItem("audioTime");
-            if (lastTime !== null) audio.currentTime = parseFloat(lastTime);
-            audio.play().catch(() => {
-            const btn = document.createElement("button");
-            btn.innerText = "play music disini ya be";
-            btn.style.position = "fixed";
-            btn.style.top = "20px";
-            btn.style.right = "20px";
-            btn.style.zIndex = "9999";
-            btn.style.padding = "10px";
-            btn.style.fontSize = "16px";
-            btn.style.borderRadius = "10px";
-            btn.style.background = "white";
-            btn.style.color = "#333";
-            btn.onclick = () => {
-        audio.play();
-        btn.remove();
-    };
-    document.body.appendChild(btn);
-});
-
-            document.querySelectorAll(".nav-form").forEach(form => {{
-                form.addEventListener("submit", e => {{
-                    e.preventDefault();
-                    document.querySelector(".page-container").classList.add("fade-out");
-                    flip.play();
-                    setTimeout(() => form.submit(), 600);
-                }});
-            }});
-        }});
-    </script>
+    <script src="/static/script.js"></script>
 '''
 
 def render_page(content, current_page, total_pages=9):
